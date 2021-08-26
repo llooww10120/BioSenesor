3c09ef145ad65a5ee461291b315ec6a299df149e
Ming <llooww10120@gmail.com>
2021-08-27 00:11:50 +0800

    test

--- test.py
+++ test.py
@@ -9,13 +9,14 @@ wb=Workbook()
 ws=wb.active
 localtime=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
 rownum=1
-while localtime !="2021-08-26 16:47:00":
+while localtime <="2021-08-26 21:20:00":
     localtime=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
     # content=str(ser.readline().decode("utf-8")[:-2])
     # if(content=="Setup Finished..."):
     ws.cell(row=rownum,column=1,value=localtime)
     for i in range(2,258):
         ws.cell(row=rownum,column=i,value=str(ser.readline().decode("utf-8")))
+        localtime=time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
     rownum+=1
     # print(str(ser.readline().decode("utf-8")[:-2]))
 print("end")
