f8706c98e501db095f1aeda8555b4361f9352de0
Ming <llooww10120@gmail.com>
2021-11-20 20:05:19 +0800

    label

--- label.py
+++ label.py
@@ -6,17 +6,18 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 
 
-df = pd.read_csv("2021-10-22.csv")
+df = pd.read_csv("./testdata/2021-10-26/1/2021-10-26.csv")
 label = df
 ax = df["time"]
 
 for i in range(len(ax)):
     for j in range(250):
         if df[str(j)][i] < 890:
-            label[str(j)][i] = 1
+            label.loc[str(j),i] = 1
         else:
-            label[str(j)][i] = 0
+            label.loc[str(j),i] = 0
 
 
 label.to_csv("label.csv")
+
 
